b78b8ae93a4674cf74988105060f9d31de0792d9
Shu Yamashita <yamashita.s.4dc5@m.isct.ac.jp>
2026-05-01 19:36:56 +0900

    [feat][reduction/cuda] Change format of cases.json.

--- reduction/cuda/run.py
+++ reduction/cuda/run.py
@@ -12,11 +12,9 @@ RESULT_DIR.mkdir(exist_ok = True)
 
 @dataclass
 class BenchmarkCase:
-    name           : str
-    source         : str
-    compiler       : str
-    compiler_flags : str
-    run_args       : str
+    name        : str
+    compile_cmd : str
+    run_cmd     : str
 
 
 def load_cases() -> list[BenchmarkCase]:
@@ -27,33 +25,26 @@ def load_cases() -> list[BenchmarkCase]:
     cases = []
     for item in raw:
         cases.append( BenchmarkCase(
-            name           = item["name"    ], 
-            source         = item["source"  ], 
-            compiler       = item["compiler"],
-            compiler_flags = item["flags"   ],
-            run_args       = item["run_args"] ) )
+            name        = item["name"       ], 
+            compile_cmd = item["compile_cmd"],
+            run_cmd     = item["run_cmd"    ] ) )
     return cases
 
 
-def compile_case(case: BenchmarkCase) -> Path:
-    src = case.source
-    exe = case.name
-    cmd = f"{case.compiler} {case.compiler_flags} {src} -o {exe}"
-    print(f"[{Path(__file__).name}] {cmd}")
+def compile_case(case: BenchmarkCase) -> None:
+    print(f"[{Path(__file__).name}] {case.compile_cmd}")
     try:
-        result = subprocess.run(f"{cmd}".split(), capture_output = True, text = True, check = True)
+        result = subprocess.run(case.compile_cmd.split(), capture_output = True, text = True, check = True)
     except subprocess.CalledProcessError as e:
         print(e.stderr, file = sys.stderr)
         sys.exit(1)
-    return Path(exe)
 
 
-def run_case(case: BenchmarkCase, exe: Path):
-    cmd = f"./{exe} {case.run_args}"
-    print(f"[{Path(__file__).name}] {cmd}")
+def run_case(case: BenchmarkCase) -> dict[str, str]:
+    print(f"[{Path(__file__).name}] {case.run_cmd}")
 
     try:
-        result = subprocess.run(cmd.split(), capture_output = True, text = True, check = True)
+        result = subprocess.run(case.run_cmd.split(), capture_output = True, text = True, check = True)
     except subprocess.CalledProcessError as e:
         print(e.stdout, file = sys.stderr)
         print(e.stderr, file = sys.stderr)
@@ -76,8 +67,8 @@ def main():
     cases = load_cases()
     results = []
     for case in cases:
-        exe = compile_case(case)
-        result = run_case(case, exe)
+        compile_case(case)
+        result = run_case(case)
         results.append(result)
     save_results(results)
 
--- reduction/cuda/test_kernel.cpp
+++ reduction/cuda/test_kernel.cpp
@@ -2,5 +2,6 @@
 
 int main()
 {
+	std::cout << 1.2 << std::endl;
 	return 0;
 }
